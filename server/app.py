fe796dbb721b1bb0991fdfc1265180b9db14e990
yvonnekadenyi-m <kadenyimasheti@gmail.com>
2026-01-16 18:07:40 +0300

    added to app.py

--- server/app.py
+++ server/app.py
@@ -20,19 +20,23 @@ def index():
 
 @app.route('/bakeries')
 def bakeries():
-    return ''
+    bakeries = Bakery.query.all()
+    return jsonify([bakery.to_dict() for bakery in bakeries]), 200
 
 @app.route('/bakeries/<int:id>')
 def bakery_by_id(id):
-    return ''
+    bakery = Bakery.query.get(id)
+    return jsonify(bakery.to_dict()), 200
 
 @app.route('/baked_goods/by_price')
 def baked_goods_by_price():
-    return ''
+    baked_goods = BakedGood.query.order_by(BakedGood.price.desc()).all()
+    return jsonify([bg.to_dict() for bg in baked_goods]), 200
 
 @app.route('/baked_goods/most_expensive')
 def most_expensive_baked_good():
-    return ''
+    baked_good = BakedGood.query.order_by(BakedGood.price.desc()).first()
+    return jsonify(baked_good.to_dict()), 200
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
